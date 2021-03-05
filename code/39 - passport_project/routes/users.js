2b3051e2da3c7069efc81b7f5862ee988c0464f2
Mikael Olsson <mikael.olsson@emmio.se>
2021-03-05 13:47:02 +0100

    Added code to login project

--- code/39 - passport_project/routes/users.js
+++ code/39 - passport_project/routes/users.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../models/users')
+
 // Login
 router.get('/login', (request, response) => {
     response.render('login')
 })
 
 router.get('/register', (request, response) => {
-    response.render('login')
+    response.render('register')
 })
 
 router.post('/login', (request, response, next) => {
@@ -15,7 +17,31 @@ router.post('/login', (request, response, next) => {
 })
 
 router.post('/register', (request, response) => {
-    // do stuff
+    const { name, email, password } = request.body
+
+    let errors = []
+
+    console.log(`Name: ${name}, Email: ${email}, Password: ${password}`)
+
+    if (!name || !email || !password) {
+        errors.push({ msg: "Please fill out all fields" })
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: "Use at least 6 characters for your password" })
+    }
+
+    if (errors.length > 0) {
+        response.render('register', {
+            errors, name, email, password
+        })
+    } else {
+        const newUser = new User({
+            name, email, password
+        })
+    }
+    response.end()
+
 })
 
 router.get('/logout', (request, response) => {
