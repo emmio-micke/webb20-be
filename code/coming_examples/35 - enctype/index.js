d5141508975c1d38c1909e4df9a88c11dc56d269
Mikael Olsson <mikael.olsson@emmio.se>
2021-03-07 22:35:51 +0100

    Restructuring folders and examples

--- code/coming_examples/35 - enctype/index.js
+++ code/coming_examples/35 - enctype/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const _ = require('lodash');
 
@@ -25,12 +24,12 @@ app.use(fileUpload({
 
 //add other middleware
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/index.html')
+app.get('/', function (request, response) {
+    response.sendFile(__dirname + '/index.html')
 });
 
 app.post('/upload-profile-pic', async (req, res) => {
